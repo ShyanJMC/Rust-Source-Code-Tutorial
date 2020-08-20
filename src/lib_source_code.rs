2a72a57a0984eefebe9ec9ee10dd6c4793cdc263
ShyanJMC <shyanjmc@shyanjmc.com>
2020-08-20 03:47:26 +0000

    v0.5.0

---
+++ src/lib_source_code.rs
@@ -0,0 +1,7 @@
+pub mod module1 {
+    pub fn separated_function1() {
+        println!("Just an example.");
+    }
+}
+// Here you can see a simply module with a function to use, is simple but usefull to understand
+// what I means with this. Return to main.rs
